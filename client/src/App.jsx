5024e723d33e7f4660ecacc1b0d37eaa527f1c28
Nat Tan <tresarpnat27@gmail.com>
2024-06-14 14:35:53 +0700

    react context api complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { createContext } from "react";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
 
+export const productContext = createContext({});
 function App() {
   const userData = {
     username: "John",
     avatar: "https://placedog.net/100/100",
     level: "platinum",
   };
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <productContext.Provider
+        value={{
+          userData,
+        }}
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </productContext.Provider>
     </div>
   );
 }
